29e6bddcd6693f477f99c6832d52b90fceef8e2d
Jacob Hampton <jhampton792@gmail.com>
2020-12-28 01:57:59 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(arr)
+  arr_of_words = arr.split(" ")
+  
+  arr_of_rev_words = []
+  
+  arr_of_words.each do |word|
+    arr_of_rev_words.push(word.reverse)
+  end
+  
+  arr_of_rev_words.join(" ")
+end
